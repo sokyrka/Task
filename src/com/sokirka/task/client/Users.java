32bfc4bd3403665f38c9591d0c605974a4f14e86
Eugine Sokirka <sokyrka_evgeniy@yahoo.com>
2015-08-11 12:17:55 +0300

    Add entry point

--- src/com/sokirka/task/client/Users.java
+++ src/com/sokirka/task/client/Users.java
@@ -1,6 +1,9 @@
 package com.sokirka.task.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.shared.HandlerManager;
+import com.google.gwt.user.client.ui.RootPanel;
 
 /**
  * @author Eugine Sokirka.
@@ -9,6 +12,9 @@ public class Users implements EntryPoint {
 
     @Override
     public void onModuleLoad() {
-
+        UsersServiceAsync rpcService = GWT.create(UsersService.class);
+        HandlerManager eventBus = new HandlerManager(null);
+        AppController appViewer = new AppController(eventBus, rpcService);
+        appViewer.go(RootPanel.get());
     }
 }
